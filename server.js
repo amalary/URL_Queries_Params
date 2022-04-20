ab7e7777e012045ce98cf07a94e1f517df8e6d2b
Anthony Malary <amalary@bu.edu>
2022-04-20 12:41:34 -0700

    Finish Express application update

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ const express = require('express');
 const app = express(); 
 const PORT = 4000; 
 // temporary, simulated database
-const products = ['t-shirt', 'shoes', 'necklace']; 
+
+const products = [
+    {
+    name: 'Boyfriend Jeans',
+    price: 168,
+    image:
+    'https://cdn.shopify.com/s/files/1/1176/4142/products/2015-06-11-Ashley_Look11_50080_23637.jpg?v=1471037731',
+  }, {
+      name: 'Knitted Cashmere Pullover',
+      price: 418.60,
+      image:
+  'https://cdn.shopify.com/s/files/1/1176/4142/products/Look_08_21926_0541_1.jpg?v=1471032586',
+  }, {
+      name: 'Azur Bracelet in Blue Azurite',
+      price: 168,
+      image:
+  'https://cdn.shopify.com/s/files/1/1176/4142/products/2015-05-08_Laydown_Look2_14120_21584.jpg?v=1471027398',
+  }, ];
 
 
 // routes/controllers
@@ -13,8 +30,15 @@ const products = ['t-shirt', 'shoes', 'necklace'];
 
 app.get('/products/:productIndex', (req,res) => {{
     res.send(products[req.params.productIndex]);
-    
+
 }});
+
+// Index Route 
+// this route will catch GET requests to /products/ and respond with all the products
+
+app.get('/products/', (req,res) => {
+    res.send(products)
+})
 
 
 app.listen(PORT, () => console.log(`Listen for client request on port ${PORT}`)); 
