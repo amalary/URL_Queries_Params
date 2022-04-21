93a70064c8edabc6f6ecbb1b89579bfda6c774d1
Anthony Malary <amalary@bu.edu>
2022-04-21 11:42:00 -0700

    Organize routes

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ const products = require('./models/product_model.js');
 // show route 
 // this route will catch GET requests to /products/index/ and respond with a single product
 
+app.get('/products/', (req,res) => {
+    res.send(products.find()); 
+}); 
+
+app.get('/products/:productId', (req,res) => {
+    products.findById(req.params.productId, (error, foundItem) => {
+        if(error) return console.log(error)
+
+        return res.send(foundItem); 
+    })
+})
+
 app.get('/products/:productIndex', (req,res) => {{
     res.send(products[req.params.productIndex]);
 
@@ -19,9 +31,6 @@ app.get('/products/:productIndex', (req,res) => {{
 // Index Route 
 // this route will catch GET requests to /products/ and respond with all the products
 
-app.get('/products/', (req,res) => {
-    res.send(products)
-})
 
 
 app.listen(PORT, () => console.log(`Listen for client request on port ${PORT}`)); 
